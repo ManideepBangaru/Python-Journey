3387d8064c4009291792f948979f8c9933c6b772
ManideepBangaru <bmd994@gmail.com>
2021-08-04 23:47:49 +0530

    made a small edit

--- PyCodes/2_StringsAndStringMethods.py
+++ PyCodes/2_StringsAndStringMethods.py
@@ -462,7 +462,7 @@ specific locations in a string
 
 """
 
-num1 = input("ener a number ")
+num1 = input("enter a number ")
 num2 = input ("enter another number ")
 prod = int (num1) * int (num2)
 print( "the product of " + str(num1 ) + " and " + str (num2) + " is " + str( prod))
